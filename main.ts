097e196ae6570b201b5bfb34580a923d8d1a84e2
crouse12 <crousekimo@yahoo.com.tw>
2019-08-26 23:40:45 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,21 @@
 //% weight=0 color=#3CB371 icon="\uf0ad"
 namespace microbitarduino {
 
+     
+      export enum digitalpin {
+        D0 = 0,
+        D1 = 1,
+        D2 = 2,
+        D3 = 3,
+        D4 = 4,
+        D5 = 5,
+        D6 = 6,
+        D7 = 7,
+        D8 = 8
+     }
      //% blockId=setpinmode block="set digital pin %pin | for %XY"
     //% weight=100
-    export function setpinmode(pin: number, XY: number): string {
+    export function setpinmode(pin: digitalpin, XY: number): string {
         return "pinMode("+pin.toString()+","+XY.toString()+")\n";    
     }
      
