e10019352c49fc834097cb49e5fc9c7321b90342
crouse12 <crousekimo@yahoo.com.tw>
2019-08-26 23:26:42 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,10 @@
 //% weight=0 color=#3CB371 icon="\uf0ad"
 namespace microbitarduino {
-        export enum pin1 {
-        //% block="0"
-        "0",
-        //% block="1"
-        "1",
-        //% block="2"
-        "2",
-        //% block="3"
-        "3",
-        //% block="4"
-        "4",
-        //% block="5"
-        "5",
-        //% block="6"
-        "6",
-        //% block="7"
-        "7",
-        //% block="8"
-        "8",
-   }
-       export enum XY1 {
-        //% block="X"
-        "X",
-        //% block="Y"
-        "Y"
-    } 
+
      //% blockId=setdigital block="Joystick's  %pin | for %XY"
     //% weight=100
-    export function setdigital(pin: pin1, XY: XY1): string {
+        //%pin
+    export function setdigital(pin: string, XY: string): string {
         return "pinMode"+pin+XY;    
     }
     
