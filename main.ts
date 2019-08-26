1180297aa0e2dc70d25d6ce222cb85fd5486e60d
crouse12 <crousekimo@yahoo.com.tw>
2019-08-26 23:31:09 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,18 @@
 //% weight=0 color=#3CB371 icon="\uf0ad"
 namespace microbitarduino {
 
-     //% blockId=setdigital block="Joystick's  %pin | for %XY"
+     //% blockId=setpinmode block="Joystick's  %pin | for %XY"
+    //% weight=100
+        //%pin
+    export function setpinmode(pin: string, XY: string): string {
+        return "pinMode("+pin.toString()+","+XY.toString()+")\n";    
+    }
+     
+     
+    //% blockId=setdigital block="Joystick's  %pin | for %XY"
     //% weight=100
         //%pin
     export function setdigital(pin: string, XY: string): string {
-        return "pinMode"+pin+XY;    
+        return "digitalWrite("+pin.toString()+","+XY.toString()+")\n";    
     }
-    
 }
