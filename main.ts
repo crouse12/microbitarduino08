1fb3e19f00349b357ddb254aa763c6f2177ec15d
crouse12 <crousekimo@yahoo.com.tw>
2019-08-27 10:53:53 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -31,26 +31,26 @@ namespace microbitnodemcu {
     }
      
      
-    //% blockId=setdigital1 block="set nodemcu digital pin  %pin | value as %XY"
+    //% blockId=setdigital1 block="set nodemcu digital pin  %pin | value to %XY"
     //% weight=101
     export function setdigital1(pin: digitalpin, XY: value):void {
         serial.writeLine("digitalWrite="+pin.toString()+","+XY.toString()+"\\n")    
     }
      
-    //% blockId=setdigital2 block="set nodemcu digital pin  %pin | PWM value as %XY"
+    //% blockId=setdigital2 block="set nodemcu digital pin  %pin | PWM value to %XY"
     //% weight=101
     export function setdigital2(pin: digitalpin, XY: number):void {
         serial.writeLine("analogWrite="+pin.toString()+","+XY.toString()+"\\n")    
     }
     
-    //% blockId=setdigital3 block="read nodemcu digital pin  %pin "
+    //% blockId=setdigital3 block="read nodemcu digital pin  %pin value"
     //% weight=101
     export function setdigital3(pin: digitalpin):string {
         serial.writeLine("digitalRead="+pin.toString()+"\\n")
         let a=serial.readUntil(serial.delimiters(Delimiters.NewLine)
         return a;
     }
-    //% blockId=setdigital4 block="read nodemcu analog pin  %pin "
+    //% blockId=setdigital4 block="read nodemcu analog pin  %pin value"
     //% weight=101 
     export function setdigital4(pin: analogpin):string {
         serial.writeLine("analogRead="+pin.toString()+"\\n")
