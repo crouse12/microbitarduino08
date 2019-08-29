af3adbe4e965abec7f1423793d83cedc33a6f487
crouse12 <crousekimo@yahoo.com.tw>
2019-08-29 22:58:31 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -61,7 +61,7 @@ namespace microbitnodemcu {
     }   
     //% blockId=thingspeak block="thingspeak  %key | field1 value %value1 | field2 value %value2 | field3 value %value3 | field4 value %value4"
     //% weight=101 value=10  key=key
-    export function thingspeak(key: string,value1: string,value2: string,value3: string,value4: string) {
+    export function thingspeak(key: string,value1: string) {
         serial.writeString("t="+key+","+value1+"\\n")
     }
 }
