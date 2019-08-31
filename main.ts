602343171c85faf60beb40f91d1c6ea88c9a25cb
crouse12 <crousekimo@yahoo.com.tw>
2019-08-31 18:26:30 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -83,8 +83,8 @@ namespace microbitnodemcu {
     }
       
     //% blockId=thingspeak2 block="thingspeak key %key | fields value %value1 "
-    //% weight=101 
-    export function thingspeak2(key:string, value1: string[]=["10","20","30"]) {
+    //% weight=101 value1.defl=["10","20","30"]
+    export function thingspeak2(key:string, value1: string[]) {
         let a=value1.length
         let b=""
         let i
